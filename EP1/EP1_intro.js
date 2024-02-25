527a1a4c960bdd3b235ce7fc798a6019cfb80efc
SATJAPHON PANYAFU <satjaphon.tonkla@gmail.com>
2024-02-25 15:46:15 +0700

    first commit

---
+++ EP1/EP1_intro.js
@@ -0,0 +1,8 @@
+//start java script
+
+//รุปแบบการเขียน js 
+/* //<Head></Head> or <body></body>
+<script type="text/javascript">
+    statement
+</script>
+*/
---
+++ EP2/app.js
@@ -0,0 +1,17 @@
+//แสดงข้อความในหน้า web
+
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+
+/*
+    แสดงข้อความในแถบ console ของ web
+ */
+
+alert("Hello mtfg!!!")
+console.log("Hello javascript")
+console.error("error virus")
+console.warn("เจอข้อผิดพลาด")
