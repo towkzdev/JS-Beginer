6b80d2ef68c2548dda035b0e5ef97c0a5e16e3d1
SATJAPHON PANYAFU <satjaphon.tonkla@gmail.com>
2024-02-25 18:13:42 +0700

    ep1-ep5

--- EP2/app.js
+++ EP2/app.js
@@ -15,3 +15,28 @@ alert("Hello mtfg!!!")
 console.log("Hello javascript")
 console.error("error virus")
 console.warn("เจอข้อผิดพลาด")
+
+
+/* ตัวแปล EP5*/
+let name,Name ="Satjaphon"; //สร้างตัวแปลแต่ไม่มีการกำหนดค่า
+name = "Dev Grrr" //การกำหนดค่าลงไปในตัวแปล name
+let age = 20
+
+console.log(name);
+document.write(name);
+console.log(Name);
+document.write(Name);
+console.log("อายุ =",age);
+
+let _for ="Kla Satjaphon";
+console.log("Befor = ",_for)
+
+_for = "Grrrrrr DevTowkz";
+console.log("After = ",_for)
+
+/* ค่าคงที่ const */
+const vat =0.07; //ค่าคงที่
+console.log("Befor = ",vat);
+
+vat = 0.089
+console.log("After = ",vat);
