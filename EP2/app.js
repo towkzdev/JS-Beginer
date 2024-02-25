0d1f8ca9dda7f7cb7c7c2a5ae1b1a08a70089f80
towkzdev <satjaphon.tonkla@gmail.com>
2024-02-26 02:00:59 +0700

    ep6-7 datatype and convert datatype

--- EP2/app.js
+++ EP2/app.js
@@ -1,23 +1,23 @@
 //แสดงข้อความในหน้า web
 
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
-document.write("<h1>Dev Grrrrrr !!!!!!!!</h1>");
+document.write("<h1>Love YOU 🩷</h1>");
+
+
 
 /*
     แสดงข้อความในแถบ console ของ web
  */
 
-alert("Hello mtfg!!!")
+//alert("Hello MP 🩷🩷🩷")
 console.log("Hello javascript")
 console.error("error virus")
 console.warn("เจอข้อผิดพลาด")
 
 
 /* ตัวแปล EP5*/
+let ep_5 ="--------------EP5---------------"
+console.log(ep_5)
+
 let name,Name ="Satjaphon"; //สร้างตัวแปลแต่ไม่มีการกำหนดค่า
 name = "Dev Grrr" //การกำหนดค่าลงไปในตัวแปล name
 let age = 20
@@ -38,5 +38,63 @@ console.log("After = ",_for)
 const vat =0.07; //ค่าคงที่
 console.log("Befor = ",vat);
 
-vat = 0.089
+//vat = 0.089
 console.log("After = ",vat);
+
+
+/* -----------EP6------------- */
+let ep_6 ="--------------EP6---------------"
+console.log(ep_6)
+
+let money = 500+500 ; //number (integer)
+let price = 99.98 ; //number (float)
+let message = "Dev Grrr";
+let NAME = "Gr Satajphon";
+
+let checkName = false ;
+let checkProduct = true ;
+
+let AGE_1 = "20" //string
+let AGE_2 = "20"+10 //string
+
+console.log(typeof(money),(money));
+console.log(typeof(price),(price));
+console.log(typeof(message),(message));
+console.log(typeof(NAME),(NAME));
+
+console.log(typeof(checkName),(checkName))
+console.log(typeof(checkProduct),(checkProduct))
+console.log(typeof(AGE_1),(AGE_1))
+console.log(typeof(AGE_2),(AGE_2))
+
+
+/* -----------EP7------------- */
+let ep_7 ="--------------EP7---------------"
+console.log(ep_7)
+
+let NTS_1 ="ep7_NumberToString"
+console.log(NTS_1)
+
+let NTS_3 = "20"
+let NTS_4 = parseInt("20")
+let NTS_5 = parseFloat("20")
+let NTS_6 = +"20"
+
+
+console.log(typeof(NTS_3),(NTS_3))
+console.log(typeof(NTS_4),(NTS_4))
+console.log(typeof(NTS_5),(NTS_5))
+console.log(typeof(NTS_6),(NTS_6)+100)
+
+let STN_1 ="ep7_StringToNumber"
+console.log(STN_1)
+
+let STN_2 = 99+""
+let STN_3 = 99.98.toString()
+
+
+
+console.log(typeof(STN_2),(STN_2))
+console.log(typeof(STN_3),(STN_3))
+
+
